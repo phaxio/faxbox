0543eb2ee2859726b6c5f9839fe154df553c77ca
Nick Verwymeren <greatwitenorth@gmail.com>
2014-07-30 13:43:44 -0500

    FAXBOX-12 - View users

---
+++ faxbox/app/views/users/edit.blade.php
@@ -0,0 +1,76 @@
+@section('content')
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Edit User</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+
+    <div class="row">
+        {{ Form::open(array('action' => 'UserController@update', 'method' => 'PUT')) }}
+
+        <div class="col-md-4">
+            <div class="form-group {{ ($errors->has('email')) ? 'has-error' : '' }}">
+                {{ Form::text('email', $user['email'], array('class' => 'form-control', 'placeholder' => trans('users.email'))) }}
+                {{ ($errors->has('email') ? $errors->first('email') : '') }}
+            </div>
+            <div class="form-group {{ ($errors->has('first_name')) ? 'has-error' : '' }}">
+                {{ Form::text('first_name', $user['first_name'], array('class' => 'form-control', 'placeholder' => trans('users.first_name'))) }}
+                {{ ($errors->has('first_name') ? $errors->first('first_name') : '') }}
+            </div>
+            <div class="form-group {{ ($errors->has('last_name')) ? 'has-error' : '' }}">
+                {{ Form::text('last_name', $user['last_name'], array('class' => 'form-control', 'placeholder' => trans('users.last_name'))) }}
+                {{ ($errors->has('last_name') ? $errors->first('last_name') : '') }}
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group">
+                <h4>User Level Permissions <small>Any permission set here will be specific to this user.</small></h4>
+                @include("partials.permissions")
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group">
+                <h4>Group Level Permissions <small>Permissions granted from groups are inherited by the user.</small></h4>
+                @foreach($groups as $group)
+                <div class="checkbox">
+                    <label>
+                        {{ Form::checkbox("groups[".$group['id']."]", 1) }} {{ $group['name'] }}
+                    </label>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-4">
+            <div class="form-group">
+                <h4>Change Password</h4>
+            </div>
+            <div class="form-group {{ ($errors->has('last_name')) ? 'has-error' : '' }}">
+                {{ Form::password('oldpassword', null, array('class' => 'form-control', 'placeholder' => trans('users.oldpassword'))) }}
+                {{ ($errors->has('last_name') ? $errors->first('last_name') : '') }}
+            </div>
+            <div class="form-group {{ ($errors->has('last_name')) ? 'has-error' : '' }}">
+                {{ Form::password('password', null, array('class' => 'form-control', 'placeholder' => trans('users.newpassword'))) }}
+                {{ ($errors->has('last_name') ? $errors->first('last_name') : '') }}
+            </div>
+            <div class="form-group {{ ($errors->has('last_name')) ? 'has-error' : '' }}">
+                {{ Form::password('password_confirmed', null, array('class' => 'form-control', 'placeholder' => trans('users.newpasswordconfirm'))) }}
+                {{ ($errors->has('last_name') ? $errors->first('last_name') : '') }}
+            </div>
+        </div>
+    </div>
+    
+    {{ Form::submit(trans('users.update'), array('class' => 'btn btn-primary')) }}
+
+    {{ Form::close() }}
+</div>
+@stop
---
+++ faxbox/app/views/users/list.blade.php
@@ -0,0 +1,59 @@
+@section('content')
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">View Users</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered table-hover" id="dataTables-fax">
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Email</th>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Created</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($users as $user)
+                    <tr class="odd gradeX">
+                        <td class="text-center"><a href="{{ action('UserController@edit', ['id' => $user['id']]) }}"><i class="fa fa-edit"> Edit</i></a></td>
+                        <td>{{ $user['email'] }}</td>
+                        <td>{{ $user['first_name'] }} {{ $user['last_name'] }}</td>
+                        <td>{{ $user['activated'] ? "<span class='label label-success'>Activated</span>" : "<span class='label label-danger'>Inactive</span>" }}</td>
+                        <td>{{ $user['created_at'] }}</td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.table-responsive -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+</div>
+@stop
+
+@section('scripts')
+<script src="{{ asset('js/plugins/dataTables/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('js/plugins/dataTables/dataTables.bootstrap.js') }}"></script>
+
+<script>
+    $(document).ready(function() {
+        $('#dataTables-fax').dataTable({
+            "order": [[ 1, "asc" ]],
+            "columnDefs": [
+                { "orderable": false, "targets": 0 },
+                { "searchable": false, "targets": 0 }
+            ]
+        });
+    });
+</script>
+@stop
