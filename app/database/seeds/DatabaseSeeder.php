fb8c84724a62dfc16a39780ec6be3a31d8a01931
Nick Verwymeren <greatwitenorth@gmail.com>
2014-07-31 15:44:08 -0500

    added dummy data

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,16 +2,31 @@
 
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
-	}
+        $this->call('DummyData');
+    }
+
+}
+
+class DummyData extends Seeder {
+
+    public function run()
+    {
+        Sentry::register([
+            'email'      => 'admin@admin.com',
+            'first_name' => 'The',
+            'last_name'  => 'Admin',
+            'password'   => Hash::make('admin'),
+            'permissions' => ['superuser' => 1]
+        ], 1);
+    }
 
 }
