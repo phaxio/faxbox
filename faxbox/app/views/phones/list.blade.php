4c86e04212c59c16d4f29aed4aa5519d96026ded
Nick Verwymeren <greatwitenorth@gmail.com>
2014-07-31 11:28:30 -0500

    FAXBOX-16 added table

---
+++ faxbox/app/views/phones/list.blade.php
@@ -0,0 +1,55 @@
+@section('content')
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">View Phone Numbers</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered table-hover" id="dataTables-fax">
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Number</th>
+                        <th>Description</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($phones as $phone)
+                    <tr class="odd gradeX">
+                        <td class="text-center"><a href="{{ action('PhoneController@edit', ['id' => $phone['id']]) }}"><i class="fa fa-edit"> Edit</i></a></td>
+                        <td>{{ $phone['number'] }}</td>
+                        <td>{{ $phone['description'] }}</td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.table-responsive -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+</div>
+@stop
+
+@section('scripts')
+<script src="{{ asset('js/plugins/dataTables/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('js/plugins/dataTables/dataTables.bootstrap.js') }}"></script>
+
+<script>
+    $(document).ready(function() {
+        $('#dataTables-fax').dataTable({
+            "order": [[ 1, "desc" ]],
+            "columnDefs": [
+                { "orderable": false, "targets": [0,2] },
+                { "searchable": false, "targets": [0,2] }
+            ]
+        });
+    });
+</script>
+@stop
