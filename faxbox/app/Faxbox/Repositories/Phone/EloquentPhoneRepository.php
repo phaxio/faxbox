459a8f5fb97dad49b61dfb60642936226fb16506
Nick Verwymeren <greatwitenorth@gmail.com>
2014-07-31 11:25:43 -0500

    FAXBOX-16 updated phone repo

--- faxbox/app/Faxbox/Repositories/Phone/EloquentPhoneRepository.php
+++ faxbox/app/Faxbox/Repositories/Phone/EloquentPhoneRepository.php
@@ -1,18 +1,50 @@
 <?php namespace Faxbox\Repositories\Phone;
 
 use Faxbox\Repositories\EloquentAbstractRepository;
+use Faxbox\Repositories\Permission\PermissionInterface;
+use Faxbox\Repositories\User\UserInterface;
 use Phaxio;
 use Faxbox\Phone;
 
 class EloquentPhoneRepository extends EloquentAbstractRepository implements PhoneInterface {
     
-    public function __construct(Phone $phone)
+    public function __construct(Phone $phone, UserInterface $users)
     {
-        $this->model = $phone;
+        $this->phones = $phone;
+//        $this->users = $users;
+//        $this->permissions = $permissions;
     }
     
     public function all()
     {
-        return $this->model->all()->toArray();
+        return $this->phones->all()->toArray();
+    }
+
+    /**
+     * Gets all phone numbers the user has access to.
+     *
+     * @param integer $userId
+     *
+     * @return array An array of phone numbers.
+     */
+    public function findByUserId($userId)
+    {
+        $user = $this->users->byId($userId);
+        
+        if($user->isSuperUser())
+        {
+            return $this->all();
+        } 
+        else
+        {
+            $permissions = $user->getMergedPermissions();
+            $allowedPhoneIds = $this->permissions->allowedResourceIds('view',
+                'Faxbox\Repositories\Phone\PhoneInterface',
+                $permissions);
+
+            $phones = $this->phones->whereIn('id', $allowedPhoneIds);
+
+            return $phones->orderBy('created_at', 'DESC')->get()->toArray();
+        }
     }
 } 
