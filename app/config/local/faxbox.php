f72dff7ca1bd7d7d3fca2a7b522ed10bd0f25d52
Nick Verwymeren <greatwitenorth@gmail.com>
2014-08-15 16:01:09 -0400

    removed ignored files

--- app/config/local/faxbox.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'installed' => true,
-    
-    'notify' => [
-        'fax' => "http://gwn.chickenkiller.com/notify/fax",
-        'send' => "http://gwn.chickenkiller.com/notify/fax/send",
-    ],
-];
